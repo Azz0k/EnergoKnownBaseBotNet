526107888d1bff34c238f52ae9d18f580de6e3ec
Павел Милокум <ya-shev4@yandex.ru>
2024-05-07 11:35:52 +0400

    First commit

---
+++ BotService.cs
@@ -0,0 +1,253 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json.Serialization;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Net.NetworkInformation;
+using System.Numerics;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Telegram.Bot.Requests.Abstractions;
+using Telegram.Bot.Types.ReplyMarkups;
+using TestWorkerService;
+using static System.Formats.Asn1.AsnWriter;
+
+namespace TestWorkerService
+{
+    public class BotService
+    {
+        public TableOFContents? _tableOfContents;
+        private IServiceProvider _serviceProvider;
+        private ILogger<WindowsBackgroundService> _logger;
+        private HashSet<long>  _cachedAutorizedIds = new HashSet<long>();
+        private AppSettings _settings;
+        public bool isDataReceived = false;
+        public BotService(IOptions<AppSettings> settings, ILogger<WindowsBackgroundService> logger, IServiceProvider serviceProvider)
+        {
+            _logger = logger;
+            _settings = settings.Value;
+            _serviceProvider = serviceProvider;
+        }
+
+        public bool IsIdAllowedDb(long id)
+        {
+            if (_cachedAutorizedIds.Contains(id))
+            {
+                return true;
+            }
+            using (var scope = _serviceProvider.CreateScope())
+            {
+                var dbContext = scope.ServiceProvider.GetRequiredService<DBRepository>();
+                bool result = dbContext.Users.AsEnumerable().Where(x => x.ID == id).Any();
+                if (result) 
+                {
+                    _cachedAutorizedIds.Add(id);
+                    return true;
+                }
+            }
+            return false;
+        }
+        public bool IsPhoneNumberAllowed(string phoneNumber, long id)
+        {
+            using(var scope = _serviceProvider.CreateScope()){
+                var dbContext = scope.ServiceProvider.GetRequiredService<DBRepository>();
+                bool result = dbContext.Users.AsEnumerable().Where(x => x.PhoneNumber.EndsWith(phoneNumber)).Any();
+                if (result)
+                {
+                    dbContext.Users.Where(x => x.PhoneNumber.EndsWith(phoneNumber)).ExecuteUpdate(users => users.SetProperty(x=>x.ID,id));
+                    //Users user = dbContext.Users.Where(x => x.PhoneNumber.EndsWith(phoneNumber)).ExecuteUpdate() ;
+                    //if (user != null) {
+                    //    user.ID = id;
+                    //}
+                    //dbContext.SaveChanges();
+                    return true;
+                }
+            }
+            return false;
+        }
+        public InlineKeyboardMarkup createStandardMarkup(string id = "")
+        {
+            string prefix = _settings.CallBackDataPrefix;
+            if (id != "") 
+            {
+                id = id.Split(' ')[1];
+            }
+            else
+            {
+                id = _settings.HeadId;
+            }
+            
+            List<List<InlineKeyboardButton>> buttons = new List<List<InlineKeyboardButton>>();
+            var currentFolder = TableOFContents.GetElementById(id); 
+            foreach (TableOFContents t in currentFolder.SubFolders.Values)
+            {
+                buttons.Add(new List<InlineKeyboardButton>(1) {
+                    new InlineKeyboardButton(t.Name)
+                    {
+                        CallbackData = prefix + t.Id
+                    }
+                });
+            }
+            foreach (File f in currentFolder.Files.Values)
+            {
+                buttons.Add(new List<InlineKeyboardButton>(1) {
+                    InlineKeyboardButton.WithUrl(f.Name, f.Url)
+                });
+            }
+            var backSymbol = Char.ConvertFromUtf32(Convert.ToInt32(_settings.BackSymbol,16));
+            var homeSymbol = Char.ConvertFromUtf32(Convert.ToInt32(_settings.HomeSymbol,16));
+
+            string backId = TableOFContents.GetElementById(id).Parent?.Id ?? _settings.HeadId;
+
+            buttons.Add(new List<InlineKeyboardButton>()
+            {
+                InlineKeyboardButton.WithCallbackData($"{backSymbol}",callbackData: prefix + backId),
+                InlineKeyboardButton.WithCallbackData($"{homeSymbol}",callbackData: prefix + _settings.HeadId),
+            }) ;
+            return new InlineKeyboardMarkup(buttons);
+
+        }
+        async public Task<Boolean> IsIdAllowed(long id) 
+        {
+            if (_cachedAutorizedIds.Contains(id))
+            {
+                return true;
+            }
+            HttpClient httpClient = new HttpClient();
+            httpClient.BaseAddress = new Uri(_settings.CheckIdUrl);
+            httpClient.DefaultRequestHeaders.Accept.Clear();
+            httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            httpClient.DefaultRequestHeaders.ConnectionClose = true;
+            var response = await (await httpClient.GetAsync(id.ToString())).EnsureSuccessStatusCode().Content.ReadAsAsync<IsAutorized>();
+            if (response.Authorization == "authorized")
+            {
+                _cachedAutorizedIds.Add(id);
+                return true;
+            }
+            return false;
+        }
+        async public Task UpdateTableAsync()
+        {
+            HttpClient httpClient = new HttpClient();
+            string url = _settings.BitrixApiUrl;
+            Uri baseUri = new Uri(url);
+            httpClient.BaseAddress = baseUri;
+            httpClient.DefaultRequestHeaders.Clear();
+            httpClient.DefaultRequestHeaders.ConnectionClose = true;
+            string clientId = _settings.BitrixApiLogin;
+            string clientSecret = _settings.BitrixApiPassword;
+            var authenticationString = $"{clientId}:{clientSecret}";
+            var base64EncodedAuthenticationString = Convert.ToBase64String(System.Text.ASCIIEncoding.ASCII.GetBytes(authenticationString));
+            var requestMessage = new HttpRequestMessage(HttpMethod.Get, "");
+            requestMessage.Headers.Authorization = new AuthenticationHeaderValue("Basic", base64EncodedAuthenticationString);
+            string response = string.Empty;
+            try
+            {
+                response = await (await httpClient.SendAsync(requestMessage)).EnsureSuccessStatusCode().Content.ReadAsStringAsync();
+                isDataReceived = true;
+                //string response = await(await httpClient.SendAsync(requestMessage)).EnsureSuccessStatusCode().Content.ReadAsStringAsync();
+                //string filePath = @"c:\temp\jsonb24.txt";
+                //string response = System.IO.File.ReadAllText(filePath);
+                Disk disk = JsonConvert.DeserializeObject<Disk>(response);
+                Root jsonRoot = JsonConvert.DeserializeObject<Root>(disk.Data);
+                TableOFContents.ClearIndex();   
+                _tableOfContents = new TableOFContents((JObject)jsonRoot.KnowledgeBase["2523"], "2523");
+            }
+            catch (Exception ex)
+            {
+                isDataReceived = false;
+                _logger.LogError(ex, "{Message}", ex.Message);
+            }
+            
+        }
+
+    }
+    public class TableOFContents 
+    {
+        public TableOFContents? Parent { get; set; }
+        public string Name = string.Empty;
+        public string Url = string.Empty; 
+        public string Id = string.Empty;
+        private static Dictionary<string, TableOFContents> _index = new();
+        public SortedList<string,TableOFContents> SubFolders = new();
+        public SortedList<string,File> Files = new();
+        public static TableOFContents GetElementById(string id) => _index[id];
+        public static void ClearIndex()
+        {
+            _index = new();
+        }
+        public TableOFContents(JObject source, string id, TableOFContents? parent = null )
+        {
+            Id = id;
+            _index.Add(Id, this);
+            Parent = parent;
+            Name = ((string)source["name"]).Replace("_"," ");
+            Url = (string)source["url"];
+            foreach (var element in source["subfolders"])
+            {
+                foreach (var inner in element)
+                {
+                    if (inner is JArray)
+                    {
+                        foreach (var jsonFile in inner)
+                        {
+                            var objFile = jsonFile.ToObject<File>();
+                            Files.Add(objFile.Name, objFile);
+                        }
+
+                    }
+                    else 
+                    {
+                        var temp = new TableOFContents((JObject)inner, ((JProperty)element).Name, this);
+                        SubFolders.Add(temp.Name, temp);
+                    }
+                    
+                }
+
+                
+            }
+                
+            
+        }
+
+    }
+    public class Disk
+    {
+        [JsonProperty("status")]
+        public required string Status { get; set; }
+        [JsonProperty("data")]
+        public required string Data { get; set; }
+        //public required Dictionary<string, JToken> Data { get; set; }
+        [JsonProperty("errors")]
+        public required List<object> Errors { get; set; }
+    }
+
+    public class Root
+    {
+        [JsonExtensionData]
+        public required Dictionary<string, JToken> KnowledgeBase { get; set; }
+    }
+
+  
+    public class File
+    {
+        [JsonProperty("id")]
+        public required string Id { get; set; }
+        [JsonProperty("name")]
+        public required string Name { get; set; }
+        [JsonProperty("url")]
+        public required string Url { get; set; }
+    }
+    public class IsAutorized 
+    {
+        [JsonProperty("authorization")]
+        public required string Authorization { get; set; }
+    }
+    
+}
---
+++ Worker.cs
@@ -0,0 +1,178 @@
+using Microsoft.Extensions.Options;
+using System.Runtime;
+using System.Threading;
+using Telegram.Bot;
+using Telegram.Bot.Exceptions;
+using Telegram.Bot.Polling;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
+using Telegram.Bot.Types.ReplyMarkups;
+
+namespace TestWorkerService
+{
+    public sealed class WindowsBackgroundService: BackgroundService
+    {
+        private readonly AppSettings _settings;
+        private readonly ILogger<WindowsBackgroundService> _logger;
+        private readonly BotService _botService;
+        private  ITelegramBotClient? _botClient;
+        private ReceiverOptions? _receiverOptions;
+
+        public WindowsBackgroundService(IOptions<AppSettings> settings, ILogger<WindowsBackgroundService> logger, BotService botService) 
+        {
+            _settings = settings.Value;
+            _logger = logger;
+            _botService = botService;
+            
+
+        }
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            try
+            {
+               
+                await _botService.UpdateTableAsync();
+                if (!_botService.isDataReceived)
+                {
+                    Environment.Exit(1);
+                }
+                _logger.LogWarning("Service started");
+                _botClient = new TelegramBotClient(_settings.TelegramBotToken);
+                _receiverOptions = new ReceiverOptions
+                {
+                    AllowedUpdates = new[]
+                    {
+                            UpdateType.Message,
+                            UpdateType.CallbackQuery,
+                    },
+                    ThrowPendingUpdates = true,
+                };
+                _botClient.StartReceiving(HandleUpdateAsync, HandleErrorAsync, _receiverOptions, stoppingToken);
+                int timer = 0;
+                while (!stoppingToken.IsCancellationRequested)
+                {
+                    await Task.Delay(1000, stoppingToken);
+                    if (++timer == 60*60) 
+                    {
+                        await _botService.UpdateTableAsync();
+                        _logger.LogWarning("The data is updated");
+                        timer = 0;
+                    }
+
+                }
+            }
+            catch (OperationCanceledException)
+            {
+                // When the stopping token is canceled, for example, a call made from services.msc,
+                // we shouldn't exit with a non-zero exit code. In other words, this is expected...
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "{Message}", ex.Message);
+                Environment.Exit(1);
+            }
+        }
+        private async Task BotMessageHandler(Telegram.Bot.Types.Message message)
+        {
+            var user = message?.From;
+            var chat = message?.Chat;
+            if (_botService.IsIdAllowedDb(user.Id)) 
+            {
+                if (message.Type == MessageType.Text)
+                {
+                    await BotStartHandler(user.Id);
+                }
+            }
+            else
+            {
+                if (message.Type == MessageType.Contact)
+                {
+                    if (_botService.IsPhoneNumberAllowed(message.Contact.PhoneNumber, user.Id))
+                    {
+                        await _botClient.SendTextMessageAsync(
+                                chatId: chat.Id,
+                                text: $"Здравствуйте, {message.Contact.FirstName}",
+                                replyMarkup: new ReplyKeyboardRemove()); 
+                        await BotStartHandler(user.Id);
+                    }
+                }
+                else
+                {
+                    ReplyKeyboardMarkup replyKeyboardMarkup = new(new[]
+                    {
+                        KeyboardButton.WithRequestContact("Отправить контактные данные"),
+                    });
+                    replyKeyboardMarkup.ResizeKeyboard = true;
+                    await _botClient.SendTextMessageAsync(chatId: chat.Id,
+                        text: _settings.GreetingText,
+                        replyMarkup: replyKeyboardMarkup);
+                }
+                
+            }
+            
+        }
+        private async Task BotCallBackHandler(Telegram.Bot.Types.CallbackQuery callbackQuery)
+        {
+            var user = callbackQuery?.From;
+            var chat = callbackQuery.Message.Chat;
+            try
+            {
+                var deleteTask = _botClient.DeleteMessageAsync(chat.Id, callbackQuery.Message.MessageId);
+                var replayTask = BotCallBackHandler(chat.Id, callbackQuery.Data);
+                await Task.WhenAll(deleteTask, replayTask);
+            }
+            catch (Exception) { }
+        }
+        private async Task BotStartHandler(long chatId)
+        {
+            
+
+            await _botClient.SendTextMessageAsync(chatId,
+                "Это бот базы знаний. Выберите раздел",
+                replyMarkup: _botService.createStandardMarkup());
+
+        }
+        private async Task BotCallBackHandler(long chatId, string callBackData)
+        {
+            if (!callBackData.StartsWith(_settings.CallBackDataPrefix))
+            {
+                await _botClient.SendTextMessageAsync(chatId,
+                "Сотрите историю и начните заново");
+                return;
+            }
+            await _botClient.SendTextMessageAsync(chatId,
+                "Нажмите на ссылку для просмотра видео",
+                replyMarkup: _botService.createStandardMarkup(callBackData));
+        }
+ 
+        async Task HandleUpdateAsync(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
+        {
+            switch (update.Type)
+            {
+                case UpdateType.Message:
+                {
+                    await BotMessageHandler(update.Message);
+                    break;
+                }
+                case UpdateType.CallbackQuery:
+                {
+                    await BotCallBackHandler(update.CallbackQuery);
+                    break;
+                }
+            }
+            
+        }
+
+        async Task HandleErrorAsync(ITelegramBotClient botClient, Exception exception, CancellationToken cancellationToken)
+        {
+            var errorMessage = exception switch
+            {
+                ApiRequestException are => $"Telegram API Error:\n[{are.ErrorCode}]\n{are.Message}",
+                _ => exception.ToString()
+            };
+            _logger.LogError(errorMessage);
+            await Task.CompletedTask;
+        }
+    }
+}
+
